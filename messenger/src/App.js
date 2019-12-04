84e7cb1164c75153361235263d8644abf592d8e9
bg-gif <goff.ben@gmail.com>
2019-12-04 09:35:56 +0000

    login page made

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import Login from "./components/Login";
+import openSocket from "socket.io-client";
+const socket = openSocket("localhost:9090");
 
-import "./App.css";
+class App extends Component {
+  state = {
+    username: "",
+    avatar: "",
+    users: [{ username: "Mary" }, { username: "palceholder" }],
+    messages: []
+  };
+  componentDidMount() {
+    socket.emit("connect");
+    socket.on("users", reply => {
+      const { users, messages } = reply;
+      this.setState({ users, messages });
+    });
+  }
 
-function App() {
-  return <div className="App"></div>;
+  handleLogin = event => {
+    const { username, avatar } = user;
+    this.setState({ username, avatar }, () => {
+      console.log(this.state);
+    });
+  };
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <Login users={users} handleLogin={this.Login} />
+      </div>
+    );
+  }
 }
 
 export default App;
